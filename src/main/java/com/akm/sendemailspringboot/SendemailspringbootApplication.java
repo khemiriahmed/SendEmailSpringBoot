c15242d8999050e3e95229fae037577079c9d310
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-13 10:07:32 +0100

    Send Email

---
+++ src/main/java/com/akm/sendemailspringboot/SendemailspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.akm.sendemailspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendemailspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendemailspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akm/sendemailspringboot/SendemailspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akm.sendemailspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendemailspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
